659135fe426d5eaa4e8f5fe1ac49df7c01bf8bbb
moh-IA <mohamed.el.arbi.rabah@gmail.com>
2021-09-26 01:53:27 +0200

    connect to cosmos_db

--- api/task-get/__init__.py
+++ api/task-get/__init__.py
@@ -1,18 +1,18 @@
-from collections import UserDict, UserList
 import logging
 import json
-from re import T
+import os
 from pymongo import MongoClient
 import azure.functions as func
 
 
 
+COSMOSDB_CONN_STRING = str(os.environ["ConnectionString"])
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    client = MongoClient("mongodb://mohamed:ji1mRUIY2iSGyYCaPcOXidzx2xhs3jSKFNSHG9rw5JCopN8I4NAVyMWsy8Uf1SWyROwsBJUteoY4epcEjad5EA%3D%3D@mohamed.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mohamed@")
+    client = MongoClient(COSMOSDB_CONN_STRING)
     database = client["workshop-swa"]
     response = database["tasks"].find(
         {'userId': "f5182bebff11b5cde1f22e34e9d736c1"})
